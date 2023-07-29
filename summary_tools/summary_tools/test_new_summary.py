b48a31fae24fa3446b317b86e5c7c125b43f910d
Ben <ben.sembira.1@gmail.com>
2023-07-29 18:21:47 +0300

    test: full system test environment

--- summary_tools/summary_tools/test_new_summary.py
+++ summary_tools/summary_tools/test_new_summary.py
@@ -12,8 +12,9 @@ def summary_template_path() -> Path:
     return Path("")  # Todo
 
 
-def add_books_to_directory(path: Path, books_names: List[str]) -> Path:
-    return path  # Todo
+def add_books_to_directory(path: Path, books_names: List[str]):
+    for book_name in books_names:
+        (path / book_name).mkdir()
 
 
 def test_generate_new_summary(empty_directory: Path, summary_template_path: Path):
@@ -22,13 +23,18 @@ def test_generate_new_summary(empty_directory: Path, summary_template_path: Path
     CHAPTER_NAME = "Chapter Name"
     SUMMARY_WRITER = "Summary Writer"
 
-    FIRST_BOOK = "First Book 2nd Edition by James and Robert"
-    SECOND_BOOK = "Second Book"
+    FIRST_BOOK = "JAMESANDROBERT_FirstBook"
+    SECOND_BOOK = "MARTINLOOTERKING_SecondBook"
 
-    directory_with_books = add_books_to_directory(
-        empty_directory, [FIRST_BOOK, SECOND_BOOK])
+    directory_with_books = empty_directory
+    add_books_to_directory(
+        directory_with_books, [FIRST_BOOK, SECOND_BOOK])
+
     cli_mock = CLIMock(options_choices=[CHOOSE_FIRST_BOOK], number_inputs_choices=[
                        CHAPTER_NUMBER], string_input_choices=[SUMMARY_WRITER, CHAPTER_NAME])
     generate_new_summary(cli_mock, directory_with_books, summary_template_path)
-    assert (directory_with_books / FIRST_BOOK /
-            f"{CHAPTER_NUMBER}_{pascal_case(CHAPTER_NAME)}_{kebab_case(SUMMARY_WRITER)}")
+
+    expected_summary_path = (directory_with_books / FIRST_BOOK /
+                             f"{CHAPTER_NUMBER}_{pascal_case(CHAPTER_NAME)}_{kebab_case(SUMMARY_WRITER)}")
+    assert expected_summary_path.exists()
+    assert expected_summary_path.is_file()
