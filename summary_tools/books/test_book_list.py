378b3513b6d2add4a2d29befd6de48358d7da7ac
Ben <ben.sembira.1@gmail.com>
2023-07-29 12:13:23 +0300

    fix: move empty directory fixture to filesystem tools

--- summary_tools/books/test_book_list.py
+++ summary_tools/books/test_book_list.py
@@ -1,19 +1,14 @@
 from pathlib import Path
-import tempfile
 import itertools
-import pytest
 
+from summary_tools.filesystem_tools.fs_test_tools import empty_directory
 from .books_search import get_all_existing_book_paths
 
-@pytest.fixture
-def empty_directory() -> Path:
-    with tempfile.TemporaryDirectory() as directory:
-        yield Path(directory)
 
 def test_list_directory_books(empty_directory: Path):
     books_directory = empty_directory
 
-    initial_books = ("AUTHOR_bookName", "AUTHORB_SomthingElseEdition2-refactored")
+    initial_books = ("AUTHORA_bookName", "AUTHORB_SomthingElseEdition2-refactored")
     not_book_names = ("something_else", ".private_things", ".WEIRD_PRIVATE")
 
     for subdir in itertools.chain(initial_books, not_book_names):
