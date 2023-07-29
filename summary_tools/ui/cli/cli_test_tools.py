26883fe102aaa7168da66e260a212eb3dea28d48
Ben <ben.sembira.1@gmail.com>
2023-07-29 14:54:29 +0300

    feat: added cli implementation

---
+++ summary_tools/ui/cli/cli_test_tools.py
@@ -0,0 +1,17 @@
+from typing import Iterable
+
+
+class InputMock:
+    def __init__(self, responses: Iterable[str]):
+        self.responses_iterator = iter(responses)
+
+    def read(self) -> str:
+        return next(self.responses_iterator)
+
+
+class OutputMock:
+    def __init__(self):
+        self.output = ""
+
+    def write(self, data: str):
+        self.output += data
