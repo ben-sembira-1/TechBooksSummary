135b9fa048bd3f97d551edd73bc5766b98959533
Ben <ben.sembira.1@gmail.com>
2023-07-29 11:22:34 +0300

    feat: wip, book re tested

---
+++ summary_tools/new_summary_test.py
@@ -0,0 +1,24 @@
+import pytest
+
+from .new_summary import book_directory_name_regex
+
+@pytest.mark.parametrize("directory_name",[
+    "A_a",
+    "A_A",
+    "ABC_aAbB",
+    "ABC_AaAa",
+    "A1_aA1Aa",
+])
+def test_book_directory_name_regex_valid_names(directory_name):
+    assert book_directory_name_regex().fullmatch(directory_name) is not None
+
+
+@pytest.mark.parametrize("directory_name",[
+    "_a",
+    "A_",
+    "Aa",
+    "a_a",
+    "AaA_a",
+])
+def test_book_directory_name_regex_invalid_names(directory_name):
+    assert book_directory_name_regex().fullmatch(directory_name) is None
