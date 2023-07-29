681e1c88372af8eb0a4a1cbbd78a7119a6e0e6ab
Ben <ben.sembira.1@gmail.com>
2023-07-29 15:11:17 +0300

    fix: fixed types

--- summary_tools/filesystem_tools/fs_test_tools.py
+++ summary_tools/filesystem_tools/fs_test_tools.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 import tempfile
+from typing import Iterator
 import pytest
 
 
 @pytest.fixture
-def empty_directory() -> Path:
+def empty_directory() -> Iterator[Path]:
     with tempfile.TemporaryDirectory() as directory:
         yield Path(directory)
