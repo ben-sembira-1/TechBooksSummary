3ef466512ed805a34fb79a60e14f4439f6584bd2
Ben <ben.sembira.1@gmail.com>
2023-07-29 16:34:46 +0300

    fix: mypy types for subpackages

--- summary_tools/summary_tools/chapter_summary/chapter_summary_metadata.py
+++ summary_tools/summary_tools/chapter_summary/chapter_summary_metadata.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 
 from summary_tools.string_tools.string_tools import kebab_case, pascal_case
 
+
 @dataclass
 class ChapterSummaryMetadata:
     writer: str
@@ -9,5 +10,5 @@ class ChapterSummaryMetadata:
     chapter_name: str
 
     def repr_no_spaces(self):
-        
+
         return f"{self.chapter_number}_{pascal_case(self.chapter_name)}__{kebab_case(self.writer)}"
