d603559db82090efb1522b2f1f11e864457fddd5
Ben <ben.sembira.1@gmail.com>
2023-07-29 11:42:56 +0300

    fix: remove underscore from book name regex

--- summary_tools/tests/book_regex_test.py
+++ summary_tools/tests/book_regex_test.py
@@ -20,6 +20,7 @@ def test_book_directory_name_regex_valid_names(directory_name):
     "a_a",
     "AaA_a",
     "A1_a",
+    "A_a_b",
 ])
 def test_book_directory_name_regex_invalid_names(directory_name):
     assert book_directory_name_regex().fullmatch(directory_name) is None
